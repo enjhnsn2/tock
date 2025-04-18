a47b5936d441ec61dbc1adc0d5e18fbb0ec24bbe
Evan Johnson <e5johnso@ucsd.edu>
2025-04-18 15:00:17 -0700

    fix previous bugs

--- libraries/tickv/src/tickv.rs
+++ libraries/tickv/src/tickv.rs
@@ -58,12 +58,13 @@ pub(crate) enum State {
 }
 
 /// The struct storing all of the TicKV information.
-// #[flux_rs::refined_by(flash_size: int)]
-// #[flux_rs::invariant(flash_size > 0)]
+#[flux_rs::refined_by(flash_size: int)]
+#[flux_rs::invariant(flash_size >= S)]
+#[flux_rs::invariant(S > 0)]
 pub struct TicKV<'a, C: FlashController<S>, const S: usize> {
     /// The controller used for flash commands
     pub controller: C,
-    // #[flux_rs::field(usize[flash_size])]
+    #[flux_rs::field(usize[flash_size])]
     flash_size: usize,
     pub(crate) read_buffer: Cell<Option<&'a mut [u8; S]>>,
     pub(crate) state: Cell<State>,
@@ -112,6 +113,7 @@ impl<'a, C: FlashController<S>, const S: usize> TicKV<'a, C, S> {
     ///
     /// `controller`: An new struct implementing `FlashController`
     /// `flash_size`: The total size of the flash used for TicKV
+    #[flux_rs::trusted]
     pub fn new(controller: C, read_buffer: &'a mut [u8; S], flash_size: usize) -> Self {
         Self {
             controller,
